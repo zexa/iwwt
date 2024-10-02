8719a3171796dd519bb2c67e75c68843c500e386
Zexa <zexa@cock.li>
2024-10-02 22:59:08 +0300

    add new book page/form

--- src/Form/NewBookFormType.php
+++ src/Form/NewBookFormType.php
@@ -16,9 +16,14 @@ class NewBookFormType extends AbstractType {
             ->add("title", TextType::class)
             ->add("author", TextType::class)
             ->add("isbn", TextType::class)
-            ->add("publicationDate", DateType::class)
+            ->add("publication_date", DateType::class)
             ->add("genres", CollectionType::class, [
                 'entry_type' => TextType::class,
+                'allow_add' => true,  // Allows adding new fields dynamically
+                'allow_delete' => true,  // Allows removing existing fields dynamically
+                'prototype' => true,  // Allows a prototype to be used for new entries
+                'by_reference' => false,  // Ensure proper handling when using collections
+                'label' => false, // Disables the label from being rendered automatically
             ])
             ->add('copies', NumberType::class)
         ;
