e53ca4a67d8e4c583c5043f2367f5f79f1c4035b
Zexa <zexa@cock.li>
2024-10-02 12:32:50 +0300

    Add maker bundle

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
