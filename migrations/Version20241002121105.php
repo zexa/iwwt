827daab45cde625f3427d57cbd8a4fd3d17d7716
Zexa <zexa@cock.li>
2024-10-02 15:11:18 +0300

    Add Book entity

---
+++ migrations/Version20241002121105.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241002121105 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE book_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE book (id INT NOT NULL, title VARCHAR(2048) NOT NULL, author VARCHAR(1024) NOT NULL, isbn VARCHAR(255) NOT NULL, publication_date DATE NOT NULL, genre TEXT NOT NULL, number_of_copies INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN book.genre IS \'(DC2Type:array)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE book_id_seq CASCADE');
+        $this->addSql('DROP TABLE book');
+    }
+}
---
+++ src/Entity/Book.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+class Book
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 2048)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 1024)]
+    private ?string $author = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $isbn = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $publicationDate = null;
+
+    #[ORM\Column(type: Types::ARRAY)]
+    private array $genre = [];
+
+    #[ORM\Column]
+    private ?int $numberOfCopies = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): static
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getIsbn(): ?string
+    {
+        return $this->isbn;
+    }
+
+    public function setIsbn(string $isbn): static
+    {
+        $this->isbn = $isbn;
+
+        return $this;
+    }
+
+    public function getPublicationDate(): ?\DateTimeInterface
+    {
+        return $this->publicationDate;
+    }
+
+    public function setPublicationDate(\DateTimeInterface $publicationDate): static
+    {
+        $this->publicationDate = $publicationDate;
+
+        return $this;
+    }
+
+    public function getGenre(): array
+    {
+        return $this->genre;
+    }
+
+    public function setGenre(array $genre): static
+    {
+        $this->genre = $genre;
+
+        return $this;
+    }
+
+    public function getNumberOfCopies(): ?int
+    {
+        return $this->numberOfCopies;
+    }
+
+    public function setNumberOfCopies(int $numberOfCopies): static
+    {
+        $this->numberOfCopies = $numberOfCopies;
+
+        return $this;
+    }
+}
