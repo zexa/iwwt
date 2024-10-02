474fd4e566e0d3b70c71371758e4e009b761328a
Zexa <zexa@cock.li>
2024-10-02 20:39:50 +0300

    fix registration/login logic

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,36 +2,94 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+use App\Form\RegistrationFormType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+    private LoggerInterface $logger;
+    private TokenStorageInterface $tokenStorage;
+
+    public function __construct(LoggerInterface $logger, TokenStorageInterface $tokenStorage)
+    {
+        $this->logger = $logger;
+        $this->tokenStorage = $tokenStorage;
+    }
+
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
+        if ($error) {
+            $this->logger->info('Failed to log in', ['error' => $error]);
+        }
 
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_books');
+        }
 
         return $this->render('security/login.html.twig', [
-            'last_username' => $lastUsername,
             'error' => $error,
         ]);
     }
 
-    #[Route(path: '/signup', name: 'app_signup')]
-    public function signup(AuthenticationUtils $authenticationUtils): Response
+    private function loginUser(UserInterface $user): void
     {
-        // get the login error if there is one
+        $token = new UsernamePasswordToken($user, 'main', $user->getRoles());
+        $this->tokenStorage->setToken($token);
+    }
+
+    #[Route(path: '/signup', name: 'app_signup')]
+    public function signup(
+        Request $request, 
+        UserPasswordHasherInterface $passwordEncoder, 
+        EntityManagerInterface $entityManager,
+        AuthenticationUtils $authenticationUtils
+    ): Response {
         $error = $authenticationUtils->getLastAuthenticationError();
+        if ($error) {
+            $this->logger->info('Failed to sign up', ['error' => $error]);
+        }
+
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_books');
+        }
+
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $passwordEncoder->hashPassword(
+                    $user,
+                    $form->get('password')->getData()
+                )
+            );
+    
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->loginUser($user);
+    
+            return $this->redirectToRoute('app_login');
+        }
 
         return $this->render('security/signup.html.twig', [
             'error' => $error,
+            'form' => $form->createView(),
         ]);
     }
 
---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('password', PasswordType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                    ]),
+                ],
+            ]);
+        ;
+    }
+}
