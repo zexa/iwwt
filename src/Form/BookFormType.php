0499b3cc65aed98bf9860bb57db83f9d3d8d0b3a
Zexa <zexa@cock.li>
2024-10-04 07:48:41 +0300

    Add missing constraints for book creation

--- src/Form/BookFormType.php
+++ src/Form/BookFormType.php
@@ -8,15 +8,45 @@ use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class BookFormType extends AbstractType {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add("title", TextType::class)
-            ->add("author", TextType::class)
-            ->add("isbn", TextType::class)
-            ->add("publication_date", DateType::class)
+            ->add("title", TextType::class, [
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'Title is required']),
+                    new Assert\Length([
+                        'max' => 2048,
+                        'maxMessage' => 'Title cannot be longer than {{ limit }} characters',
+                    ]),
+                ],
+            ])
+            ->add("author", TextType::class, [
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'Author is required']),
+                    new Assert\Length([
+                        'max' => 1024,
+                        'maxMessage' => 'Author name cannot be longer than {{ limit }} characters',
+                    ]),
+                ],
+            ])
+            ->add("isbn", TextType::class, [
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'ISBN is required']),
+                    new Assert\Regex([
+                        'pattern' => '/^(?:\d{9}[\dXx]|\d{13})$/',
+                        'message' => 'The ISBN must be valid (ISBN-10 or ISBN-13)',
+                    ]),
+                ],
+            ])
+            ->add("publication_date", DateType::class, [
+                'widget' => 'single_text',
+                'constraints' => [
+                    new Assert\Date(['message' => 'Please enter a valid date']),
+                ],
+            ])
             ->add("genres", CollectionType::class, [
                 'entry_type' => TextType::class,
                 'entry_options' => ['label' => false],
@@ -26,8 +56,19 @@ class BookFormType extends AbstractType {
                 'prototype_name' => '__name__',
                 'by_reference' => false,
                 'label' => false,
+                'constraints' => [
+                    new Assert\Count([
+                        'min' => 1,
+                        'minMessage' => 'You must specify at least one genre',
+                    ]),
+                ],
             ])
-            ->add('copies', NumberType::class)
+            ->add('copies', NumberType::class, [
+                'constraints' => [
+                    new Assert\NotBlank(['message' => 'Number of copies is required']),
+                    new Assert\PositiveOrZero(['message' => 'Number of copies must be zero or positive']),
+                ],
+            ])
         ;
     }
 }
